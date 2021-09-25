fe9593373c35d6fa8ed1cf2bd8604f8ac351a6d1
Süli Ákos <akossuli@protonmail.com>
2021-09-25 19:44:17 +0200

    fixed input algorithm and replaced the dimension input function with a while loop

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -1,37 +1,25 @@
 n = 0
 m = 0
 
-def row_coloumn_input():
-    global m, n
+print("Provide the dimensions of the matrix:")
+while n < 2 or n > 10 or m < 2 or m > 10:
     n = input("n = ")
     n = int(n)
     m = input("m = ")
     m = int(m)
     if n < 2 or n > 10 or m < 2 or m > 10:
-        print("Both dimensions of the matrix must be between 1 and 10.\n")
-        row_coloumn_input()
-
-print("Provide the dimensions of the matrix:")
-row_coloumn_input()
-if n > m:
-    print("Since the number of rows is greater than the number of coloumns, Gaussian elimination can not be performed on this matrix.\n")
-    row_coloumn_input()
+        print("Both dimensions of the matrix must be between 2 and 10.\n")
 
 #The matrix is stored as nested list
 matrix = []
-
-def row_input(num_of_coloumns):
-    row = list(map(int, input(f"Enter the values separated with spaces: ").split()))
-    if len(row) != num_of_coloumns:
-        print(f"You need to enter exactly {num_of_coloumns} values.\n")
-        print(row)
-        row_input(num_of_coloumns)
+i = 1
+while i <= m:
+    row = list(map(int, input(f"\nEnter the values of row number {i} separated with spaces: ").split()))
+    if len(row) == n:
+        row.append(int(input("Enter the term right of the line in the augmented matrix: ")))
+        matrix.append(row)
+        i += 1
     else:
-        return(row)
+        print(f"You need to enter exactly {n} values.")
 
-def matrix_input(num_of_rows, num_of_coloumns):
-    global matrix
-    for i in range(1, num_of_rows + 1):
-        print(f"\nRow number {i}:")
-        matrix.append(row_input(num_of_coloumns))
-        print(matrix)
+print(matrix)
