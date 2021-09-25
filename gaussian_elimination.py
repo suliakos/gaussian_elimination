7e715eb346881a2eb278a614a19d04b47ba34f8e
Süli Ákos <akossuli@protonmail.com>
2021-09-25 16:30:08 +0200

    fixed the order of dimensions

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -13,7 +13,7 @@ def row_coloumn_input():
 
 print("Provide the dimensions of the matrix:")
 row_coloumn_input()
-if m > n:
+if n > m:
     print("Since the number of rows is greater than the number of coloumns, Gaussian elimination can not be performed on this matrix. Please try again.\n")
     row_coloumn_input()
 
