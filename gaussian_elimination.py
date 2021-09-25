90361e22c60c9b21ff3905f018a075e2d9021671
Süli Ákos <akossuli@protonmail.com>
2021-09-25 19:19:59 +0200

    added matrix input which is in wip

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -7,15 +7,31 @@ def row_coloumn_input():
     n = int(n)
     m = input("m = ")
     m = int(m)
-    if n < 1 or n > 10 or m < 1 or m > 10:
-        print("Both dimensions of the matrix must be between 1 and 10. Please try again.\n")
+    if n < 2 or n > 10 or m < 2 or m > 10:
+        print("Both dimensions of the matrix must be between 1 and 10.\n")
         row_coloumn_input()
 
 print("Provide the dimensions of the matrix:")
 row_coloumn_input()
 if n > m:
-    print("Since the number of rows is greater than the number of coloumns, Gaussian elimination can not be performed on this matrix. Please try again.\n")
+    print("Since the number of rows is greater than the number of coloumns, Gaussian elimination can not be performed on this matrix.\n")
     row_coloumn_input()
 
 #The matrix is stored as nested list
 matrix = []
+
+def row_input(num_of_coloumns):
+    row = list(map(int, input(f"Enter the values separated with spaces: ").split()))
+    if len(row) != num_of_coloumns:
+        print(f"You need to enter exactly {num_of_coloumns} values.\n")
+        print(row)
+        row_input(num_of_coloumns)
+    else:
+        return(row)
+
+def matrix_input(num_of_rows, num_of_coloumns):
+    global matrix
+    for i in range(1, num_of_rows + 1):
+        print(f"\nRow number {i}:")
+        matrix.append(row_input(num_of_coloumns))
+        print(matrix)
